d3123f8b211ca84feb66a62583a4ffd3cf346e08
Mason <75508453+skw359@users.noreply.github.com>
2024-07-31 20:47:27 -0400

    Potentially make it more straightforward

--- ShimmeringText.swift
+++ ShimmeringText.swift
@@ -3,18 +3,16 @@ import SwiftUI
 
 struct ShimmeringText: View {
     var text: String
-    var baseColor: Color
-    var shimmerColor: Color = .white
     @State private var gradientPosition: CGFloat = -1
     
     var body: some View {
         ZStack {
             Text(text)
-                .foregroundColor(baseColor)
-                .overlay(
+                .foregroundColor(.white)
+                .background(
                     GeometryReader { geometry in
                         LinearGradient(
-                            gradient: Gradient(colors: [shimmerColor.opacity(0), shimmerColor, shimmerColor.opacity(0)]),
+                            gradient: Gradient(colors: [Color.white.opacity(0), Color.white, Color.white.opacity(0)]),
                             startPoint: .leading,
                             endPoint: .trailing
                         )
